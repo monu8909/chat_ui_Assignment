a2f6c649c1ab5dea9ab1d447d8d842c77aaa73bf
Monu Rajput <monurajput89099@gmail.com>
2023-04-02 23:58:07 +0530

    add the code

--- src/App.js
+++ src/App.js
@@ -4,20 +4,6 @@ import Chat from "../src/ChatFolder/Chat";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={"/Profile4.png"} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <Chat />
     </div>
   );
